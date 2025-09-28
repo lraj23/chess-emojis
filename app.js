a01dc02580c48ff359ba217f487819e9ff826e1b
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-09-27 21:12:29 -0700

    make bot react based on AI prompt

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 import app from "./client.js";
+const aiApiUrl = "https://openrouter.ai/api/v1/chat/completions";
+const headers = {
+	"Authorization": `Bearer ${process.env.CEMOJIS_AI_API_KEY}`,
+	"Content-Type": "application/json"
+};
+const reaction_emojis = [
+	"chess-brilliant",
+	"great",
+	"bestmove",
+	"excellent_move",
+	"good_move",
+	"book_move",
+	"inaccuracy",
+	"real-chess-mistake",
+	"real-chess-missed-win",
+	"blunder"
+]
 
 app.message('', async ({ message, say }) => {
+	const response = await fetch(aiApiUrl, {
+		method: "POST",
+		headers,
+		body: JSON.stringify({
+			"model": "openai/gpt-oss-120b",
+			"messages": [
+				{
+					"role": "system",
+					"content": "The user message consists of a message sent in a conversation. Your job is to analyze the message and determine how it would fit as a chess move. For example, a common question or statement would be a book move. The best response to a question or statement would likely be a best move. A response that might not be the best but is still good and understandable would be an excellent move. A simply acceptable response would be a good move. A message that is a bit of a mistake or worse than a good move would be an inaccuracy. Worse than that, a mistake. Even worse, a blunder. A message that is unexpected but much better than the expected best move is a great move. A message that is very unexpected, brings more information, and is far beyond the expected best message would be considered a brilliant move. Finally, a message that had a really obvious best move that wasn't said could possibly be a miss instead. What you HAVE to do is respond EXACTLY with one of these following strings according to your best analysis: " + reaction_emojis.join(", ") + "."
+				},
+				{
+					"role": "user",
+					"content": message.text
+				}
+			]
+		})
+	});
+	const data = await response.json();
+	console.log(data.choices[0].message);
 	await app.client.reactions.add({
 		"channel": message.channel,
-		"name": "chess-brilliant",
+		"name": (res => (reaction_emojis.includes(res) ? res : "error_web"))(data.choices[0].message.content),
 		"timestamp": message.ts
-	})
+	});
+
 });
 
 app.message('secret button', async ({ message, say }) => {
